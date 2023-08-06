6be2a90be43640d66fe66fa9b88e3074ca0e9072
ireero <78822225+ireero@users.noreply.github.com>
2023-08-06 20:30:01 -0300

    first commit

---
+++ mult_screens_flutter_course/android/app/src/main/kotlin/com/example/mult_screens_flutter_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mult_screens_flutter_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mult_screens_flutter_course/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mult_screens_flutter_course/categories.dart';
+
+final theme = ThemeData(
+    // brightness: Brightness.dark,
+    useMaterial3: true,
+    textTheme: GoogleFonts.latoTextTheme(),
+    colorScheme:
+        ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 131, 57, 0)));
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
+}
