768f2e261996eab76035f4d8689b52fabfdcf09a
ireero <78822225+ireero@users.noreply.github.com>
2023-08-07 20:49:01 -0300

    Avançando no módulo do curso.

--- mult_screens_flutter_course/lib/main.dart
+++ mult_screens_flutter_course/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mult_screens_flutter_course/data/dummy_data.dart';
 import 'package:mult_screens_flutter_course/screens/categories.dart';
+import 'package:mult_screens_flutter_course/screens/meals.dart';
 
 final theme = ThemeData(
     // brightness: Brightness.dark,
@@ -20,7 +22,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const MealsScreen(
+        title: 'Some category...',
+        meals: dummyMeals,
+      ),
     );
   }
 }
---
+++ mult_screens_flutter_course/lib/screens/meals.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:mult_screens_flutter_course/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({required this.title, required this.meals, super.key});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (context, index) => Text(meals[index].title),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uh oh... nothing here!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Try selecting a difrrent category!',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
+}
