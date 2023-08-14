1ccbb67e91ae9e769f643cb70fc4224f49dd850e
ireero <78822225+ireero@users.noreply.github.com>
2023-08-14 01:20:14 -0300

    Avançando no curso.

--- mult_screens_flutter_course/lib/screens/meals.dart
+++ mult_screens_flutter_course/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mult_screens_flutter_course/models/meal.dart';
+import 'package:mult_screens_flutter_course/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({required this.title, required this.meals, super.key});
@@ -14,30 +15,10 @@ class MealsScreen extends StatelessWidget {
       itemBuilder: (context, index) => Text(meals[index].title),
     );
 
-    if (meals.isEmpty) {
-      content = Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Uh oh... nothing here!',
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              'Try selecting a difrrent category!',
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            )
-          ],
-        ),
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
       );
     }
 
